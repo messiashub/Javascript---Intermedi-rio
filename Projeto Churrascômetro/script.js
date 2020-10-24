0d5ec0eb9c4e517aa1f0d96957fd34b1aab5adae
claudinei messias <claudineimessias@hotmail.com>
2020-10-23 23:33:54 -0300

    Estrutura html

---
+++ Projeto Churrascômetro/script.js
@@ -0,0 +1,4 @@
+// Carne - 400 gr por pessoa + de 6 horas - 650
+// Cerveja - 1200 ml por pessoa + 6 horas  - 2000 ml
+// refrigerante/agua - 1000ml por pessoa + 6 horas 1500ml
+// crianças valem  por 0,5
