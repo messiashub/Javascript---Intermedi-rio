b7510ca8d75da331d544a771bb43df63a0018a0b
messiashub <claudineimessias@hotmail.com>
2021-06-17 22:03:36 -0300

    update

--- telaDeCadastro/script.js
+++ telaDeCadastro/script.js
@@ -5,15 +5,19 @@ let Mvalidador = {
         let enviar = true;
 
         let inputs = form.querySelectorAll('input');
+
+        Mvalidador.clearErrors();
+
         for(let i=0; i < inputs.length; i++){
             let input = inputs[i];
             let check = Mvalidador.checkInput(input);
             if(check !== true){
                 enviar = false;
                 // exibir erro
-            } 
-            console.log(check);
+                Mvalidador.showError(input,check);
 
+            } 
+            
         }
         
         if(enviar){
@@ -40,8 +44,30 @@ let Mvalidador = {
             }
         }
         return true;
+    },
+    showError:(input,error)=>{
+        input.style.borderColor='red';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+        input.parentElement.insertBefore(errorElement,input.ElementSibling);
+ 
+
+    },
+
+    clearErrors:()=>{
+        let inputs = form.querySelectorAll('input');
+        for(let i= 0; i < inputs.length;i++){
+            inputs[i].style = '';
+        }
+
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0; i < errorElements.length; i++){
+            errorElements[i].remove()
+        }
     }
 
-}
+};
 let form = document.querySelector('.validador');
 form.addEventListener('submit',Mvalidador.handleSubmit);
