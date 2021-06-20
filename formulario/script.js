3e14af3bd96b31dae3206ece9fdb81ca88ab1869
messiashub <claudineimessias@hotmail.com>
2021-06-20 01:40:14 -0300

    update

---
+++ formulario/script.js
@@ -0,0 +1,10 @@
+
+// 1 -> Variáveis Globais
+
+let btnAd = document.querySelector('.adicionar');
+let dataNasc = document.querySelector('input[name="ano"]');
+let anoAtual = new Date().getFullYear()
+
+// Funções
+
+// Rotina Princpal
