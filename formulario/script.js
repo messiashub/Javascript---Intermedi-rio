0e72c3b0e4cd6eb514cdcdc7a3af353e48c7be0e
messiashub <claudineimessias@hotmail.com>
2021-06-20 15:52:23 -0300

    update

--- formulario/script.js
+++ formulario/script.js
@@ -6,5 +6,23 @@ let dataNasc = document.querySelector('input[name="ano"]');
 let anoAtual = new Date().getFullYear()
 
 // Funções
+function adicionar_dados(event){
+    event.preventDefault();
+    let ano_informado = dataNasc.value;
+    if(ano_informado >= 1900 && ano_informado <= anoAtual){
+         document.querySelector('.alerta').innerHTML = '';
+        console.log('deu certo')
+
+    }else{
+        document.querySelector('.alerta').innerHTML = 'Deu errado';
+    }
+}
 
 // Rotina Princpal
+btnAd.addEventListener('click', adicionar_dados);
+
+
+
+
+// anotações:
+// preventDefault, newDate
