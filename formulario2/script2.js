df9d7fcf1aa402cfc031ad0aa241d0793f927cd1
messiashub <claudineimessias@hotmail.com>
2021-06-23 23:57:12 -0300

    update

--- formulario2/script2.js
+++ formulario2/script2.js
@@ -5,14 +5,23 @@ let dataAno = document.querySelector('input[name="ano"]');
 let btnAd = document.querySelector('.adicionar');
 let mensagem = document.querySelector('.mensagem');
 let anoAtual = new Date().getFullYear();
+let idade = ()=>{
+    return anoAtual - dataAno.value;
+}
 
-btnAd.addEventListener('click',(event)=>{
+
+ 
+btnAd.addEventListener('click', (event) => {
     event.preventDefault();
-    if(dataAno >= 1900 && dataAno <= anoAtual){
-         mensagem.innerHTML = `O nome é ${dataNome.value} e o ano é ${dataAno.value}`;
-    }else{
-        mensagem.innerHTML= 'error'
+    if (dataAno.value >= 1900 && dataAno.value <= anoAtual) {
+        /* let idade = anoAtual - dataAno.value */
+
+        mensagem.style.fontSize = '1.5em'
+        mensagem.innerHTML = `O nome é ${dataNome.value}, ano de seu nascimento é ${dataAno.value} e sua idade é ${idade()} anos`;
+    } else {
+        mensagem.style.fontSize = '1.5em'
+        mensagem.innerHTML = 'error'
     }
-   
-    
+
+
 })
